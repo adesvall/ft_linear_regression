5a41899ffc9a4ac59e9ab551312324f63e58e17d
adesvall <adesvall@student.42.fr>
2023-03-14 15:15:51 +0100

    alreadyfullbonuses

--- predict.py
+++ predict.py
@@ -3,9 +3,9 @@ import pandas as pd
 def estimate_price(theta0, theta1, km):
     return theta0 + theta1 * km
 
-# sauvegarde des donnees dans result.csv
+# Recuperation des donnees depuis result.csv
 resfile = pd.read_csv('result.csv')
 theta0, theta1 = float(resfile['theta0']), float(resfile['theta1'])
 
-km = int(input('Mileage :'))
-print(estimate_price(theta0, theta1, km))
+km = int(input('Mileage: '))
+print('Estimated price:', estimate_price(theta0, theta1, km))
--- train.py
+++ train.py
@@ -1,20 +1,34 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import time
 
 learning_rate = 0.1
 nb_iter = 1000
 
 def visualize(X, Y, a, b):
-    plt.title('Root Mean Squared Error according to its mileage')
+    plt.title('Price / Mileage')
+    plt.ylabel('Price')
+    plt.xlabel('Mileage')
     plt.scatter(X, Y)
-    plt.plot(X, a*X+b)
+    plt.plot(X, a*X+b, color='red')
     plt.show()
 
 def prompt_loss(RMSE):
-    plt.title('Root Mean Squared Error / iterations')
-    plt.plot(RMSE)
+    print('----------------------------------------')
+    ploted = []
+    for i in range(len(RMSE)):
+        if i % 10 == 0:
+            print('\rRMSE =', RMSE[i], end='')
+            ploted.append(RMSE[i])
+            plt.clf()
+            plt.title(f'Root Mean Squared Error / iterations\ncurrent RMSE = {RMSE[i]:.3} (iteration ' + str(i) + ')')
+            plt.ylabel('RMSE')
+            plt.xlabel('Iterations')
+            plt.plot([i*10 for i in range(len(ploted))], ploted)
+            plt.pause(0.1)
     plt.show()
+    print()
 
 a = 0
 b = 0
@@ -25,6 +39,7 @@ X = np.array(df['km'])
 Y = np.array(df['price'])
 coef_X = X.max()
 coef_Y = Y.max()
+# scaling
 X = X / coef_X
 Y = Y / coef_Y
 
@@ -35,10 +50,19 @@ for i in range(nb_iter):
     grad_b = (a * X + b - Y).mean()
     grad_a = ((a * X + b - Y) * X).mean()
     loss = ((a * X + b - Y) **2 ).mean()
+    RMSE.append(loss**0.5 * coef_Y)
 
     b -= learning_rate * grad_b
     a -= learning_rate * grad_a
-    RMSE.append(loss**0.5 * coef_Y)
+
+loss = ((a * X + b - Y) **2 ).mean()
+RMSE.append(loss**0.5 * coef_Y)
+
+# sauvegarde des donnees dans result.csv
+resfile = pd.read_csv('result.csv')
+resfile['theta0'] = b * coef_Y
+resfile['theta1'] = a * coef_Y / coef_X
+resfile.to_csv('result.csv', index=False)
 
 def bonus():
     print('MSE =', ((a * X + b - Y) **2 ).mean() * coef_Y ** 2)
@@ -49,10 +73,5 @@ def bonus():
     visualize(X * coef_X, Y * coef_Y, a * coef_Y / coef_X, b * coef_Y)
     prompt_loss(RMSE)
 
-# sauvegarde des donnees dans result.csv
-resfile = pd.read_csv('result.csv')
-resfile['theta0'] = b * coef_Y
-resfile['theta1'] = a * coef_Y / coef_X
-resfile.to_csv('result.csv', index=False)
 
 bonus()
