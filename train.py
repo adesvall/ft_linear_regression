5bf4962045e95776082966549c52a2ef0e6bbdc7
Augustin DESVALLEES <adesvall@e3r13p11.clusters.42paris.fr>
2023-03-15 12:10:51 +0100

    chaud

--- train.py
+++ train.py
@@ -3,36 +3,11 @@ import pandas as pd
 import numpy as np
 import time
 
-learning_rate = 0.1
+learning_rate = 0.2
 nb_iter = 1000
-
-def visualize(X, Y, a, b):
-    plt.title('Price / Mileage')
-    plt.ylabel('Price')
-    plt.xlabel('Mileage')
-    plt.scatter(X, Y)
-    plt.plot(X, a*X+b, color='red')
-    plt.show()
-
-def prompt_loss(RMSE):
-    print('----------------------------------------')
-    ploted = []
-    for i in range(len(RMSE)):
-        if i % 10 == 0:
-            print('\rRMSE =', RMSE[i], end='')
-            ploted.append(RMSE[i])
-            plt.clf()
-            plt.title(f'Root Mean Squared Error / iterations\ncurrent RMSE = {RMSE[i]:.3} (iteration ' + str(i) + ')')
-            plt.ylabel('RMSE')
-            plt.xlabel('Iterations')
-            plt.plot([i*10 for i in range(len(ploted))], ploted)
-            plt.pause(0.1)
-    plt.show()
-    print()
 
 a = 0
 b = 0
-
 
 df = pd.read_csv('data.csv')
 X = np.array(df['km'])
@@ -44,19 +19,24 @@ X = X / coef_X
 Y = Y / coef_Y
 
 RMSE = [] # Root Mean Squared Error Tab
+THETA = [] # Theta Tab
 
 # learning...
 for i in range(nb_iter):
     grad_b = (a * X + b - Y).mean()
     grad_a = ((a * X + b - Y) * X).mean()
-    loss = ((a * X + b - Y) **2 ).mean()
-    RMSE.append(loss**0.5 * coef_Y)
+    # save history
+    if i < 10 or (i < 200 and i % 10 == 0) or i % 100 == 0:
+        loss = ((a * X + b - Y) **2 ).mean()
+        RMSE.append(loss**0.5 * coef_Y)
+        THETA.append([i, a * coef_Y / coef_X, b * coef_Y])
 
     b -= learning_rate * grad_b
     a -= learning_rate * grad_a
 
 loss = ((a * X + b - Y) **2 ).mean()
 RMSE.append(loss**0.5 * coef_Y)
+THETA.append([nb_iter, a * coef_Y / coef_X, b * coef_Y])
 
 # sauvegarde des donnees dans result.csv
 resfile = pd.read_csv('result.csv')
@@ -64,14 +44,46 @@ resfile['theta0'] = b * coef_Y
 resfile['theta1'] = a * coef_Y / coef_X
 resfile.to_csv('result.csv', index=False)
 
+
+
+def visualize(X, Y, THETA, RMSE):
+    # plt.size = (20, 10)
+    # plt.tight_layout()
+    fig, axs = plt.subplots(2, 1, figsize=(15, 15))
+    main, err = axs
+    fig.tight_layout(pad=3)
+    itetab = []
+    ploted = []
+    for i in range(len(THETA)):
+        ite, a, b = THETA[i]
+
+        main.clear()
+        main.set_ylabel('Price ($)')
+        main.set_xlabel('Mileage (km)')
+        main.set_title('Price / Mileage (iteration ' + str(ite) + ')')
+        main.scatter(X, Y)
+        main.plot(X, a*X+b, color='red')
+
+        rmse = RMSE[i]
+        print('\rRMSE =', rmse, end='')
+        itetab.append(ite)
+        ploted.append(rmse)
+        err.clear()
+        err.set_title(f'Root Mean Squared Error / iterations    (current RMSE = {rmse:.5}$)')
+        err.set_ylabel('RMSE ($)')
+        err.set_xlabel('Iterations')
+        err.plot(itetab, ploted)
+        plt.pause(0.2)
+
+    print()
+    plt.show()
+
 def bonus():
     print('MSE =', ((a * X + b - Y) **2 ).mean() * coef_Y ** 2)
     print('RMSE =', ((a * X + b - Y) **2 ).mean()**0.5 * coef_Y)
     # pour avoir une idee plus precise (qui ne depend plus de l'unité -> %), on peut calculer la RSE (Relative Squared Error)
     print('RSE =', ((a * X + b - Y) **2 ).mean() / ((Y - Y.mean())**2).mean())
 
-    visualize(X * coef_X, Y * coef_Y, a * coef_Y / coef_X, b * coef_Y)
-    prompt_loss(RMSE)
-
+    visualize(X * coef_X, Y * coef_Y, THETA, RMSE)
 
 bonus()
